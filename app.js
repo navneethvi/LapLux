1baa78e468b9c4ce23bfae2ccfc2256b95b4361b
Navaneeth V <navaneethvinod18@gmail.com>
2023-12-22 09:26:53 +0530

    bug fix

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(session({
     resave : false,
     saveUninitialized : false,
     cookie : {
-        maxAge : 72 * 60 * 60 * 1000, //Expires in 72 hours
+        maxAge : 72 * 60 * 60 * 1000,
         httpOnly : true
     }
 }))
